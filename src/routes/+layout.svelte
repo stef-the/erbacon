c45b666eaf313c7d766a46dd2155f02d56c2b499
stef-the <stefthedoggo4@gmail.com>
2025-03-15 11:57:07 +0000

    small tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,10 +27,10 @@
 				label: 'Services',
 				href: '/services',
 				children: [
-					{ label: 'Construction Products', href: '/construction' },
-					{ label: 'Generators and Power Solutions', href: '/generators' },
-					{ label: 'Used Equipment', href: '/used' },
-					{ label: 'Temporary Fencing', href: '/fencing' }
+					{ label: 'Construction Products', href: 'services/construction' },
+					{ label: 'Generators and Power Solutions', href: 'services/generators' },
+					{ label: 'Used Equipment', href: 'services/used' },
+					{ label: 'Temporary Fencing', href: 'services/fencing' }
 				]
 			}
 		],
